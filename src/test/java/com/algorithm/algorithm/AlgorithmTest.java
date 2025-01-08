4fefc2b3f65f0effba00bfcf36e8168cb50b47bb
Phương Linh <160749794+phuonglinh81@users.noreply.github.com>
2025-01-08 14:45:48 +0700

    Update function calculateFibonacci

--- src/test/java/com/algorithm/algorithm/AlgorithmTest.java
+++ src/test/java/com/algorithm/algorithm/AlgorithmTest.java
@@ -3,8 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.algorithm.algorithm;
-import static com.algorithm.algorithm.Algorithm.Bubblesort;
-import static com.algorithm.algorithm.Algorithm.Fibonaccicalculate;
+import static com.algorithm.algorithm.Algorithm.bubbleSort;
+import static com.algorithm.algorithm.Algorithm.calculateFibonacci;
 import static com.algorithm.algorithm.Algorithm.isPrime;
 import java.util.Arrays;
 import java.util.List;
@@ -19,72 +19,75 @@ public class AlgorithmTest {
     @Test
     void testBubbleSort() {
         int[] array = {5, 2, 9, 1, 6};
-        Algorithm.Bubblesort(array);
+        Algorithm.bubbleSort(array);
         assertArrayEquals(new int[]{1, 2, 5, 6, 9}, array);
     }
 
     @Test
     void testBubbleSortSortedArray() {
         int[] array = {1, 2, 3, 4, 5};
-        Algorithm.Bubblesort(array);
+        Algorithm.bubbleSort(array);
         assertArrayEquals(new int[]{1, 2, 3, 4, 5}, array); 
     }
     
     @Test
     void testBubbleSortArrayWithDuplicates() {
         int[] array = {5, 3, 5, 1, 2};
-        Algorithm.Bubblesort(array);
+        Algorithm.bubbleSort(array);
         assertArrayEquals(new int[]{1, 2, 3, 5, 5}, array); 
     }
 
     @Test
     void testBubbleSortEmptyArray() {
         int[] array = {};
-        Algorithm.Bubblesort(array);
+        Algorithm.bubbleSort(array);
         assertArrayEquals(new int[]{}, array); 
     }
 
     @Test
     void testBubbleSortSingleElementArray() {
         int[] array = {10};
-        Algorithm.Bubblesort(array);
+        Algorithm.bubbleSort(array);
         assertArrayEquals(new int[]{10}, array); 
     }
     @Test
     void testBubbleSortWithNegativeNumbers() {
         int[] arr = {-5, -1, -3, -2, -4};
         int[] expected = {-5, -4, -3, -2, -1};
-        Algorithm.Bubblesort(arr);
+        Algorithm.bubbleSort(arr);
         assertArrayEquals(expected, arr);
     }
     @Test
     void testBubbleSortWithLargeInput() {
-        int[] arr = new int[1000000];
-        Arrays.setAll(arr, i -> (int)(Math.random() * 1000000));
+        int[] arr = new int[1000];
+        Arrays.setAll(arr, i -> (int)(Math.random() * 1000));
         int[] sortedArr = arr.clone();
         Arrays.sort(sortedArr);
-        Algorithm.Bubblesort(arr);
+        Algorithm.bubbleSort(arr);
         assertArrayEquals(sortedArr, arr);
     }
     // Test Fibonacci
     @Test
     void testFibonacci() {
-        assertEquals(8, Fibonaccicalculate(6));
+        assertEquals(8, calculateFibonacci(6));
     }
     @Test
     void testFibonacciZero() {
-        assertEquals(0, Fibonaccicalculate(0)); 
+        assertEquals(0, calculateFibonacci(0)); 
     }
     @Test
     void testFibonacciOne() {
-        assertEquals(1, Fibonaccicalculate(1)); 
+        assertEquals(1, calculateFibonacci(1)); 
     }   
     @Test
     void testFibonacciLarge() {
-        assertEquals(55, Fibonaccicalculate(10)); 
+        assertEquals(832040, calculateFibonacci(30)); 
     }
-    
-    
+    @Test
+    void testFibonacciWithNegativeNumber() {
+        assertThrows(IllegalArgumentException.class, () -> calculateFibonacci(-1));
+    }
+      
     // Test Prime Check
     @Test
     void testIsPrime() {
